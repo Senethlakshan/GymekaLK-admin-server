2cc800cbf57fa39b2704bf3a94cf9982454a960d
Senethlakshan <81217773+Senethlakshan@users.noreply.github.com>
2023-06-10 23:56:35 +0530

    update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const connectDB = require('./db/conn');
 const routes = require('./router/routes');
 const manager = require('./router/manager');
 const branch = require('./router/branches');
+const package = require('./router/package');
 
 const app = express();
 connectDB();
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use('/api', routes);
 app.use('/api', manager);
 app.use('/api', branch);
+app.use('/api', package);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
